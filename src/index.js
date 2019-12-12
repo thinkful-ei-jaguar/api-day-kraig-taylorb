ceb3797159a7bec3e768129e265f08368ffa498e
Taylor Bradshaw <taylordyoung1@gmail.com>
2019-12-12 12:12:57 -0500

    completed step 6

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import $ from 'jquery';
 import api from './api';
+import store from './store';
 import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
+ 
   api.getItems()
     .then(res => res.json())
     .then(res => console.log(res));
